5ae40668aa9a3e9fc1afcd17b86f53d8ee5e63d1
chris burge and carrie schmid <chris@>
2019-11-14 09:00:50 -0800

    add scaffolding to project.

---
+++ src/components/App.jsx
@@ -0,0 +1,16 @@
+
+import React from 'react';
+import { Switch, Route } from 'react-router-dom';
+
+function App() {
+  return (
+    <div> 
+        <NavBar/>
+      
+        <Location/>
+        <OurTeam/>
+    
+    </div>
+  );
+}
+export default App;
